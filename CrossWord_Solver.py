456d6d71a4d785af96839f988bc313652a38d665
guytamari <guy8267@gmail.com>
2024-01-16 14:05:07 +0200

    Pretty Dont - Without GUI

--- CrossWord_Solver.py
+++ CrossWord_Solver.py
@@ -1,8 +1,7 @@
 from urllib.parse import quote
 import sys
-import requests, urllib
+import requests
 from bs4 import BeautifulSoup
-import pprint
 import re
 import pdb
 
@@ -10,21 +9,13 @@ import pdb
 # second_url= solutions == '_' between words
 
 
-#               Things to fix:
-#                       1:      printing שתי מילים  Y
-#                       2:
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) '
                   'Chrome/91.0.4472.124 Safari/537.36'}
 url = 'https://www.note.co.il/solution/'
 second_url = 'https://www.note.co.il/solutions/'
 number_pattern = re.compile(r'\d+(\.\d+)?')
-user_hint = sys.argv[1][::-1]
-num_of_letters = int(sys.argv[2])
-# combined_hint = ' '.join(user_hint)
-final_hint = user_hint.replace(' ', '-')[::-1]
-final_hint2 = user_hint.replace(' ', '_')[::-1]
+word = re.compile("םילימ")
 
 
 # first url req
@@ -39,25 +30,24 @@ def req(hint):
 def req2(hint):
     full_url = second_url + quote(hint)
     res = requests.get(full_url, headers=headers)
-    # print(full_url)
     return res
 
 
 # modifying answers
 def answers(soup):
-    div = soup.find(class_='dictionary origin_content').text
+    div = soup.find(class_='dictionary origin_content').text.split('\n')
     if div:
-        div = div.split('\n')
-        # reversing the strings
+        corrected_sentences_list = []
         for sentence in div:
-            corrected_sentences_list = sentence[::-1]
+
+            corrected_sentences_list.append(sentence)
             # search for number in the strings
-            match = number_pattern.search(corrected_sentences_list)
-            if match:
-                if int(match.group(0)) == num_of_letters:
-                    print(sentence[::-1])
-            if match is None:
-                print('שתי מילים או יותר'[::-1])
+            match = number_pattern.search(sentence)
+            words = word.search(sentence)
+            if match and words and letter_or_word == '+':
+                print(sentence)
+            elif match.group() == letter_or_word:
+                print(sentence)
             else:
                 pass
     else:
@@ -72,12 +62,17 @@ def main():
     if not_found:
         # using second url
         soup = BeautifulSoup(req2(final_hint2).text, 'html.parser')
-        print('using second url req')
         answers(soup)
     else:
-        print('using first url req')
         answers(soup)
 
 
 if __name__ == '__main__':
+    print('-------------Hello, Welcome to my CrossWord Solver-------------\n'
+          '---------------------------------------------------------------\n')
+    user_hint = input('-------------Enter Your Given Hint------------- ')[::-1]
+    letter_or_word = input('-------------Enter The Number of Letters Given (for 2 + words enter W)------------- ')
+
+    final_hint = user_hint.replace(' ', '-')[::-1]
+    final_hint2 = user_hint.replace(' ', '_')[::-1]
     main()
